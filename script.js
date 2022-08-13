d4fe7f55c3781e9a57257c8d4235b84ff42dfa8a
SUREENASLAWI <sureen130@gmail.com>
2022-08-13 14:08:57 +0300

    cw6

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 alert("اهلا بك في موقعي الدي سيحسب درجتك")
 let grade = prompt("اكتب درجتك بالارقام")
 
-console.log("grade")
+console.log( "%cgrade","color:red")
 
-if(grade>=90 && grade<100) {
+if(grade>=90 ) {
     console.log("لقد حصلت على امتياز 🥳")
-}else if(grade>=80 && grade<100 ){
+}else if(grade>=80  ){
     console.log("لقد حصلت على جيد جداً🤩")
-}else if(grade>=70 && grade<100 ){
+}else if(grade>=70  ){
     console.log("لقد حصلت على جيد🙂")
-}else if(grade>=60 && grade<100){
+}else if(grade>=60){
     console.log("لقد حصلت على مقبول😕")
-}else if(grade>=50 && grade<100){
+}else if(grade>=50 ){
     console.log("لقد حصلت على ضعيف☹️")
 }else{
     console.log("راسب💔")
 }
+//جافاسكربت ليست جافا
+
